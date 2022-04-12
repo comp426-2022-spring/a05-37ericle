a01efba50737d71546af4c1a26b9f2a92b27a822
ericle <ericle@email.unc.edu>
2022-04-11 22:00:47 -0400

    fixed single flip coin png

--- index.js
+++ index.js
@@ -83,7 +83,6 @@ app.post('/app/flip/coins', (req, res, next) => {
 
 app.post('/app/flip/call', (req, res, next) => {
     const game = flipACoin(req.body.choice)
-    console.log(req.body.guess)
     res.status(200).json(game)
 })
 
